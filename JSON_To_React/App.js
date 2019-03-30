71a6a5cfe7add49bdccee06b6c1468468473ba0d
Unknown <bjchoi514@gmail.com>
2019-03-30 15:24:37 -0700

    Updated Generator
    The program was fixed to accommodate the JSON file given from Google Cloud Platform. Updated JSON file. Working on CSS Format.

--- JSON_To_React/App.js
+++ JSON_To_React/App.js
@@ -1,76 +1,117 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import $ from 'jquery';
 
-function callPythonCode(path) {
-  $.ajax({
-    type: "POST",
-    url: path
-  }).done(function( o ) {
-     console.log(o);
-     console.log("herro");
-  });
+function Page(props) {
+
+  var page = {
+    blocks: [],
+    dimensions: {
+      width:props.data.size.width,
+      height:props.data.size.height,
+      aspectRatio: props.data.size.height/props.data.size.width
+    },
+
+  };
+
+  for (var i = 0; i < props.data.blocks.length; i++) {
+    page.blocks.push({
+        paragraphs:[],
+        coordinates: {
+          left_top: {
+            x: props.data.blocks[i].left_top_v.x,
+            y: props.data.blocks[i].left_top_v.y
+          },
+          right_bot: {
+            x: props.data.blocks[i].right_bot_v.y,
+            y: props.data.blocks[i].right_bot_v.y
+          }
+        }
+    });
+
+    for (var j = 0; j < props.data.blocks[i].paragraphs.length; j++) {
+      page.blocks[i].paragraphs.push({
+        element:props.data.blocks[i].paragraphs[j].para_str,
+        coordinates: {
+          left_top: {
+            x: props.data.blocks[i].paragraphs[j].left_top_v.x,
+            y: props.data.blocks[i].paragraphs[j].left_top_v.y
+          },
+          right_bot: {
+            x: props.data.blocks[i].paragraphs[j].right_bot_v.y,
+            y: props.data.blocks[i].paragraphs[j].right_bot_v.y
+          }
+        }
+      });
+
+    }
+
+  }
+
+  return (
+    <body>
+        <div className='page' style={{
+          paddingBottom: page.dimensions.aspectRatio*100 + '%',
+          width: '70%',
+          float: 'center'
+        }}>
+        {
+          page.blocks.map(block => (
+            console.log(page.dimensions.aspectRatio*100),
+            <div className='block' style={{
+              position:'relative',
+              left:block.coordinates.left_top.x * (page.dimensions.aspectRatio*document.maxWidth),
+              top:block.coordinates.right_bot.y * (page.dimensions.aspectRatio*document.maxHeight)
+            }}>
+              {
+                block.paragraphs.map(paragraph => (
+                  console.log( paragraph.coordinates.left_top.x* (document.maxWidth)),
+                  <p style={{
+                    fontSize:'200px',
+                    position:'relative',
+                    color: 'black',
+                    left:paragraph.coordinates.left_top.x* (page.dimensions.aspectRatio*document.maxWidth),
+                    top:paragraph.coordinates.right_bot.y* (page.dimensions.aspectRatio*document.maxHeight)
+                  }}>
+                  {paragraph.element}
+                  </p>
+                ))
+              }
+            </div>
+
+          ))
+        }
+        </div>
+      </body>
+  );
+
 }
 
-function generateHTMLTags(item) {
-    var dict = {
-      titles: [],
-      paragraphs: []
-    };
-    var itemCount = 0;
-    var subCount = 0;
-
-    item.titles.map( title => {
-      var name = "head" + itemCount.toString() + "-" + subCount.toString();
-      var left = title.coordinate.x[0] + 'px';
-      var top = title.coordinate.y[0] + 'px';
-
-      const tStyle = {
-        left: left,
-        top: top,
-        position: 'absolute'
-      }
-
-      dict.titles.push(<h1 id="{{name}}" style={tStyle}>{title.element}</h1>);
-      subCount++;;
-    });
-
-    subCount = 0;
-    item.paragraphs.map(paragraph => {
-      var name = "paragraph" + itemCount.toString() + "-" + subCount.toString();
-      var left = paragraph.coordinate.x[0] + 'px';
-      var top = paragraph.coordinate.y[0] + 'px';
-
-      const pStyle = {
-        left: left,
-        top: top,
-        position: 'absolute'
-      }
-
-      dict.paragraphs.push(<p id="{{name}}" style={pStyle}>{paragraph.element}</p>);
-      subCount++;
-
-
-    });
-
-    return dict;
+function loadData (path) {
+  try {
+    var data = require('/Users/jaminapps/Documents/Programming/react/json-to-webpage/src/out.json');
+    console.log(data);
+    return require('/Users/jaminapps/Documents/Programming/react/testparse/src/out.json');
   }
+  catch(err) {
+    console.log('error');
+  }
+}
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      items: {},
-      isLoaded:false,
+      items:{},
+      isLoaded:false
     }
   }
 
   componentDidMount() {
 
     try {
-      var data = require('/Users/jaminapps/Documents/Programming/react/reactapi/src/tdata.json')
+      var data = require('/Users/jaminapps/Documents/Programming/react/testparse/src/out.json');
       this.setState({
         item: data,
         isLoaded:true,
@@ -79,42 +120,29 @@ class App extends Component {
     catch(err) {
       console.log('error');
     }
+
   }
 
   render() {
 
-      var { isLoaded, item } = this.state;
+    var { isLoaded, item } = this.state;
+    console.log(item);
 
-      console.log(this.state.item)
+    if (!isLoaded) {
+      return (
+        <div>Loading...</div>
 
-      var path = "/Users/jaminapps/Documents/Projects/NoteSquireFiles/jsonWrite.py ";
-      callPythonCode(path);
-
-      const bStyle  = {
-        position:'static'
-      }
-
-      const dStyle = {
-
-      }
-
-      if (!isLoaded) {
-          return <div>Loading...</div>;
-      }
-      else {
-        var htmlTags = generateHTMLTags(item);
-        return (
-          <body id="body" sytle={bStyle}>
-            <div>
-              {htmlTags.titles}
-              {htmlTags.paragraphs}
-            </div>
-          </body>
-        );
-      }
-
+      )
+    }
+    else {
+          return (
+            <Page data={item} />
+          );
+    }
 
   }
+
+
 }
 
 export default App;
