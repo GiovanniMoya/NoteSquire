ced66053e7d872569b56b8bbed51e1099739dc0d
bin315a1 <bin315a1@gmail.com>
2019-03-30 21:03:31 +0000

    diagram detection added - text within image not taken care of yet

--- readerModule/tester_main.py
+++ readerModule/tester_main.py
@@ -5,9 +5,10 @@ def main():
 #    parser.add_argument("path", type=str)
 #    args = parser.parse_args()
 #    path = args.path
-    path = '../../sample_images/withTag_1.jpg'
-    path_cp = ir.createImageCopy(path)
-    ir.detect_document(path_cp)
-
+    path = './temp/withTagIndex_6.JPG'
+#    path_cp = ir.createImageCopy(path)
+#    ir.detect_document(path_cp)
+#    ir.first_run(path_cp)
+    ir.imageReader(path)
 main()
     
